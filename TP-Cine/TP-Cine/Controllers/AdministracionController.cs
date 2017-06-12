85383ae84fffe70ad3faa4d2500e1c263464d14d
deymus95 <deymus95@gmail.com>
2017-06-12 16:21:54 -0300

    cambios en el contexto

--- TP-Cine/TP-Cine/Controllers/AdministracionController.cs
+++ TP-Cine/TP-Cine/Controllers/AdministracionController.cs
@@ -13,6 +13,43 @@ namespace TP_Cine.Controllers
 
         public ActionResult Inicio()
         {
+            Entities ctx = new Entities();
+            var generos = (from Generos in ctx.Generos
+                           select Generos);
+            if (generos.Count() == 0)
+            {
+                Generos g1 = new Generos();
+                Generos g2 = new Generos();
+                Generos g3 = new Generos();
+
+                g1.Nombre = "Terror";
+                g2.Nombre = "Thriller";
+                g3.Nombre = "Acción";
+
+                ctx.Generos.Add(g1);
+                ctx.Generos.Add(g2);
+                ctx.Generos.Add(g3);
+            }
+
+            var calificaciones = (from Calificaciones in ctx.Calificaciones
+                                  select Calificaciones);
+            if (calificaciones.Count() == 0)
+            {
+                Calificaciones c1 = new Calificaciones();
+                Calificaciones c2 = new Calificaciones();
+                Calificaciones c3 = new Calificaciones();
+
+                c1.Nombre = "ATP";
+                c2.Nombre = "mayores de 13";
+                c3.Nombre = "mayores de 13 con reservas";
+
+                ctx.Calificaciones.Add(c1);
+                ctx.Calificaciones.Add(c2);
+                ctx.Calificaciones.Add(c3);
+            }
+
+            ctx.SaveChanges();
+
             return View();
         }
 
@@ -23,7 +60,17 @@ namespace TP_Cine.Controllers
 
         public ActionResult Sedes()
         {
-            return View();
+            List<Sedes> todasSedes = new List<Sedes>();
+
+            Entities ctx = new Entities();
+
+            var listaSedes = (from sedes in ctx.Sedes
+                              select sedes).ToList();
+
+            todasSedes = (List<Sedes>)listaSedes;
+
+            return View(todasSedes);
+            //return View();
         }
 
         public ActionResult Carteleras()
@@ -37,26 +84,38 @@ namespace TP_Cine.Controllers
         }
 
         //Agregar pelicula nueva
-        public ActionResult AgregarPelicula()
+        [HttpPost]
+        public ActionResult AgregarPelicula(FormCollection form)
         {
-            return View();
+            Entities ctx = new Entities();
+            Peliculas peli = new Peliculas();
+
+            peli.Nombre = form["nombre"];
+            peli.Descripcion = form["descripcion"];
+            //peli.Calificaciones = form["calificacion"];
+            //peli.Generos = form["genero"];
+            peli.Imagen = form["imagen"];
+            peli.Duracion = Convert.ToInt16(form["duracion"]);
+
+            return View("Peliculas");
         }
 
         //Agregar sede nueva
         [HttpPost]
         public ActionResult AgregarSede(FormCollection form)
         {
-            CineEntities ctx = new CineEntities();
+            Entities ctx = new Entities();
             Sedes sede = new Sedes();
 
             sede.Nombre = form["nombre"];
             sede.Direccion = form["direccion"];
             sede.PrecioGeneral = Convert.ToDecimal(form["precioGeneral"]);
 
-            ctx.Sedess.Add(sede);
+            ctx.Sedes.Add(sede);
             ctx.SaveChanges();
 
-            return View("Sedes");
+            //return View("Sedes");
+            return RedirectToAction("Sedes");
         }
 
         //Agregar cartelera nueva
