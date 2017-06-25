331bf8efebe447e4a9bc747b7a94a64c87e4839c
Pablo Hernandez <herpablo88@gmail.com>
2017-06-24 20:39:54 -0700

    Agregadas validaciones. PENDIENTE: Imagen

--- TP-Cine/TP-Cine/Models/Peliculas.cs
+++ TP-Cine/TP-Cine/Models/Peliculas.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace TP_Cine
 {
+    [MetadataType(typeof(PeliculaMetadata))]
     public partial class Peliculas
     {
+
     }
 }
