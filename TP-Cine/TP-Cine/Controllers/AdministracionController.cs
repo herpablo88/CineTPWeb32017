87400fb9431dfc2a9d91824081e12dc1cfb7bfbf
deymus95 <deymus95@gmail.com>
2017-06-25 15:10:25 -0300

    validacion de sede

--- TP-Cine/TP-Cine/Controllers/AdministracionController.cs
+++ TP-Cine/TP-Cine/Controllers/AdministracionController.cs
@@ -14,6 +14,7 @@ namespace TP_Cine.Controllers
     public class AdministracionController : Controller
     {
         CineNegocio CN = new CineNegocio(); //Si lo creamos aca no es necesario agregarlo en todas las acciones
+        Entities ctx = new Entities();
        
         //
         // GET: /Administracion/
@@ -32,13 +33,13 @@ namespace TP_Cine.Controllers
             return View(CN.listaSedes);
         }
 
-        [HttpPost]
-        public ActionResult AgregarSede(FormCollection form)
-        {
-            CN.agregarSede(form["nombre"],form["direccion"],Convert.ToDecimal(form["precioGeneral"]));
-            
-            return RedirectToAction("Sedes");
-        }
+        //[HttpPost]
+        //public ActionResult AgregarSede(FormCollection form)
+        //{
+        //    CN.agregarSede(form["nombre"], form["direccion"], Convert.ToDecimal(form["precioGeneral"]));
+
+        //    return RedirectToAction("Sedes");
+        //}
 
         public ActionResult Sedes_Modificar(int id)
         {
@@ -97,7 +98,7 @@ namespace TP_Cine.Controllers
         }
         
 
- //Agregar pelicula nueva
+        //Agregar pelicula nueva
         [HttpPost]
         public ActionResult AgregarEditarPelicula(FormCollection form)
         {
@@ -153,6 +154,22 @@ namespace TP_Cine.Controllers
             return View();
         }
 
-       
+        //Estos dos ActionResult son de prueba
+        public ActionResult AgregarSede()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AgregarSede(FormCollection form)
+        {
+            //if (!ModelState.IsValid)
+            //{
+                CN.agregarSede(form["Nombre"], form["Direccion"], Convert.ToDecimal(form["PrecioGeneral"]));
+                return RedirectToAction("Sedes");
+            //}
+            //return RedirectToAction("AgregarSede");
+        }
     }
 }
