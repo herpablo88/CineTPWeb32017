0635727ad0f5e7035d9a68dd0b274a234875b689
RocioBravo <bravomaggie@gmail.com>
2017-06-25 05:13:38 -0300

    Reserva (falta validar la fecha)

---
+++ TP-Cine/TP-Cine/js/reserva.js
@@ -0,0 +1,22 @@
+﻿    $(document).ready(function () {
+        //Evento change --> Seleccioné una versión 
+        $("#VersionReserva").change(function () {
+            $("#SedeReserva").empty();
+            $.ajax({
+                type: 'POST',
+                url: '../obtenerSedeReserva', // llamada del método json
+                dataType: 'json',
+                data: { id: $("#VersionReserva").val() },
+                success: function (sedes) {
+                    // sedes tiene la lista en formato Json que se creó en el método
+                    $.each(sedes, function (i, sede) {
+                        $("#SedeReserva").append('<option value="' + sede.Value + '">' + sede.Text + '</option>');
+                    }); // Se agregan las opciones al dropdownlist SedeReserva
+                },
+                error: function (ex) {
+                    alert('Error al cargar sedes');
+                }
+            });
+            return false;
+        })
+    });
