940272328b4140a96a32df7c06977cc58a3e2f2e
RocioBravo <bravomaggie@gmail.com>
2017-06-30 00:38:26 -0300

    Reserva - Seleccionar Sede

--- TP-Cine/TP-Cine/js/reserva.js
+++ TP-Cine/TP-Cine/js/reserva.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
                 data: { id: $("#VersionReserva").val() },
                 success: function (sedes) {
                     // sedes tiene la lista en formato Json que se creó en el método
+                    $("#SedeReserva").append('<option >Seleccione Sede</option>');
                     $.each(sedes, function (i, sede) {
                         $("#SedeReserva").append('<option value="' + sede.Value + '">' + sede.Text + '</option>');
                     }); // Se agregan las opciones al dropdownlist SedeReserva
